626ab17f742d72d52a19e69efc18754f7c1e6ddc
fcfangcc <swjfc22@live.com>
2025-02-27 15:18:54 +0800

    docs with mike
    Signed-off-by: fcfangcc <swjfc22@live.com>

---
+++ docs/README.md
@@ -0,0 +1,6 @@
+###
+
+```
+mike deploy -u v0.4.0 latest
+mike deploy -u v0.4.0a1 dev
+```
--- scripts/mk-gh-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-pip install -e ".[doc]"
-cd ./docs && mkdocs gh-deploy
